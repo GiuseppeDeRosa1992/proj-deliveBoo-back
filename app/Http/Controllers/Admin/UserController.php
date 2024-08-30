e60c1ee916f348fbfe8519e8439e3d7d2be44542
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-08-30 16:51:39 +0200

    implemented controller  User show,store,update,destroy

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -22,48 +22,84 @@ class UserController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
-        //
-    }
+    public function create() {}
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'name' => 'required',
+            'email' => 'required|min:10',
+            'password' => 'required',
+        ]);
+
+        //aggiungo slug al progetto
+        //$data['slug'] = Str::slug($request->title, '-');
+
+        //creo variabile dove metto il percorso per lo storage dove vanno a finire le immagini che prendo dal create e poi le attacco alla variabile data dove passo tutti i dati del validate
+        //$img_path = Storage::put('images', $request['img_preview']);
+        //$data['img_preview'] = $img_path;
+
+
+        $newUser = new User();
+
+        $newUser->fill($data);
+        $newUser->save();
+
+        //dopo che ho slavato come nel seeder gli passo i linguaggi stavolta a mano tramite il create con le checkbox
+        // $newProject->languages()->sync($data['languages']);
+
+
+        return redirect()->route('admin.users.index');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(User $user)
     {
-        //
+        $data = [
+            'users' => $user
+        ];
+        return view('admin.users.show', $data);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(User $user)
     {
-        //
+        $data = [
+            'users' => $user
+        ];
+        return view('admin.users.edit', $data);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, User $user)
     {
-        //
+        $data = $request->validate([
+            'name' => 'required',
+            'email' => 'required|min:10',
+            'password' => 'required',
+        ]);
+
+        $user->update($data);
+
+        return redirect()->route('admin.users.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        return redirect()->route('admin.users.index');
     }
 }
---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+	<h2>ciao sono edit</h2>
+@endsection
