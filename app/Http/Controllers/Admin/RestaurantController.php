c30d799fdc8b11ffe6bc856ab3d0250e2e6aa4c3
francesco madonna <160396542+francescomadonna712@users.noreply.github.com>
2024-08-30 17:30:40 +0200

    implement restaurant controller, index, edit,create,show,update,destroy

--- app/Http/Controllers/Admin/RestaurantController.php
+++ app/Http/Controllers/Admin/RestaurantController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Restaurant;
 use Illuminate\Http\Request;
+use illuminate\Support\Facades\Storage;
+use illuminate\Support\Str;
 
 class RestaurantController extends Controller
 {
@@ -12,7 +15,10 @@ class RestaurantController extends Controller
      */
     public function index()
     {
-        //
+        $data = [
+            'restaurants' => Restaurant::orderByDesc('id')->paginate(9)
+        ];
+        return view('admin.restaurants.index', $data);
     }
 
     /**
@@ -20,7 +26,8 @@ class RestaurantController extends Controller
      */
     public function create()
     {
-        //
+
+        return view('admin.restaurants.create');
     }
 
     /**
@@ -28,38 +35,90 @@ class RestaurantController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'user_id' => 'required',
+            'name' => 'required|min:10',
+            'image' => 'required',
+            'p_iva' => 'required',
+            'address' => 'required'
+        ]);
+
+        //aggiungo slug al progetto
+        //$data['slug'] = Str::slug($request->title, '-');
+
+        //creo variabile dove metto il percorso per lo storage dove vanno a finire le immagini che prendo dal create e poi le attacco alla variabile data dove passo tutti i dati del validate
+        //$img_path = Storage::put('images', $request['img_preview']);
+        //$data['img_preview'] = $img_path;
+
+
+        $newRestaurant = new Restaurant();
+
+        $newRestaurant->fill($data);
+        $newRestaurant->save();
+
+        //dopo che ho slavato come nel seeder gli passo i linguaggi stavolta a mano tramite il create con le checkbox
+        // $newProject->languages()->sync($data['languages']);
+
+
+        return redirect()->route('admin.restaurants.index');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Restaurant $restaurant)
     {
-        //
+        $data = [
+            'restaurants' => $restaurant
+        ];
+        return view('admin.restaurants.show', $data);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Restaurant $restaurant)
     {
-        //
+        $data = [
+            'restaurants' => $restaurant
+        ];
+        return view('admin.restaurants.edit', $data);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Restaurant $restaurant)
     {
-        //
+        $data = $request->validate([
+            'user_id' => 'required',
+            'name' => 'required|min:10',
+            'image' => 'required',
+            'p_iva' => 'required',
+            'address' => 'required'
+        ]);
+
+        if ($request->has('image')) {
+            // save the image
+            $img_path = Storage::put('images', $request['image']);
+            $data['image'] = $img_path;
+            if ($restaurant->image && !Str::startsWith($restaurant->image, 'http')) {
+                Storage::delete($restaurant->image);
+            }
+        }
+
+        $restaurant->update($data);
+
+        return redirect()->route('admin.restaurants.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Restaurant $restaurant)
     {
-        //
+        $restaurant->delete();
+
+        return redirect()->route('admin.restaurants.index');
     }
 }
