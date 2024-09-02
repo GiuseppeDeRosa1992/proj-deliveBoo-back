cb386e5b24010ad7d702a0906aee848033c009e0
Lorenzo D'Addazio <ldaddazio11@gmail.com>
2024-09-02 15:12:34 +0200

    added crate restaurant and fixed bug images

--- resources/views/admin/restaurants/show.blade.php
+++ resources/views/admin/restaurants/show.blade.php
@@ -4,7 +4,8 @@
 	<div class="col-4 mt-3 m-auto">
 		<div class="card p-0 h-100">
 			<div class="col-12 text-center">
-				<img src="{{ $restaurants->image }}" class="img-fluid rounded-start" alt="immagine-ristorante">
+				<img src="{{ url('storage/' . $restaurants->image ) }}" class="img-fluid rounded-start" alt="immagine-ristorante">
+
 			</div>
 			<div class="row g-0 m-0">
 				<div class="col-12">
