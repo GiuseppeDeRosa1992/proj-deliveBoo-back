3d9c7a033ffb47f67ac8d541c52d00098780659e
Lorenzo D'Addazio <ldaddazio11@gmail.com>
2024-09-02 17:47:55 +0200

    added visualization of old values in edit restaurant

--- resources/views/admin/restaurants/edit.blade.php
+++ resources/views/admin/restaurants/edit.blade.php
@@ -24,7 +24,7 @@
 			{{-- Step 1: Nome Ristorante --}}
 			<div class="mb-3">
 				<label for="title" class="form-label">Come si chiama il tuo ristorante?</label>
-				<input type="text" class="form-control" name="name" value="{{ old('name') }}">
+				<input type="text" class="form-control" name="name" value="{{ old('name', $restaurants->name) }}">
 				@error('name')
 					<div class="form-text text-danger">{{ $message }}</div>
 				@enderror
@@ -33,7 +33,7 @@
             {{-- Step 2: Immagine Ristorante --}}
 			<div class="mb-3">
 				<label for="image" class="form-label">Scegli una nuova immagine per il tuo ristorante</label>
-				<input type="file" class="form-control" name="image" placeholder="">
+				<input type="file" class="form-control" name="image">
 				@error('image')
 					<div class="form-text text-danger">The Image field is required.</div>
 				@enderror
@@ -42,7 +42,7 @@
             {{-- Step 3: PIva Ristorante --}}
             <div class="mb-3">
 				<label for="description" class="form-label">Qual'è la tua Partita Iva?</label>
-				<input type="text" class="form-control" name="p_iva" value="{{ old('p_iva') }}">
+				<input type="text" class="form-control" name="p_iva" value="{{ old('p_iva', $restaurants->p_iva) }}">
 				@error('p_iva')
 					<div class="form-text text-danger">{{ $message }}</div>
 				@enderror
@@ -51,7 +51,7 @@
             {{-- Step 4: Indirizzo Ristorante --}}
 			<div class="mb-3">
 				<label for="address" class="form-label">Qual'è il tuo nuovo indirizzo del ristorante</label>
-				<input type="text" class="form-control" name="address" value="{{ old('address') }}">
+				<input type="text" class="form-control" name="address" value="{{ old('address', $restaurants->address) }}">
 				@error('address')
 					<div class="form-text text-danger">{{ $message }}</div>
 				@enderror
