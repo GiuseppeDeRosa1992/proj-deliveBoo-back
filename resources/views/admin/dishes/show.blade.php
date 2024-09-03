65b3fe7e729360e47e72e345a28c8a5352707089
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-09-03 11:32:38 +0200

    fix bug src img

--- resources/views/admin/dishes/show.blade.php
+++ resources/views/admin/dishes/show.blade.php
@@ -1,19 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-12 p-0">
-                <img src="{{ $dishes->image  }}" class="w-100" style="height:50vh" />
-            </div>
-            <div class="col-12 p-4">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-12 p-0">
+				<img src="{{ asset('storage/' . $dishes->image) }}" class="w-50" style="height:50vh" />
+			</div>
+			<div class="col-12 p-4">
 
-                <h2>{{ $dishes->name }}</h2>
-                <p> {{ $dishes->description}} </p>
-                <p> {{ $dishes->price }} </p>
+				<h2>{{ $dishes->name }}</h2>
+				<p> {{ $dishes->description }} </p>
+				<p> {{ $dishes->price }} </p>
 
-            </div>
-        </div>
-    </div>
-
+			</div>
+		</div>
+	</div>
 @endSection
