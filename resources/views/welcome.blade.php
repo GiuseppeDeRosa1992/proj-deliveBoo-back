3e120f37362e8cd6be779a70e1262d2708d3352d
sidane17 <khalilsidane89@gmail.com>
2024-09-04 13:14:50 +0200

    spazio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,6 @@
 @section('content')
     <div class="py-5 jumbo text-center">
         <h1 class="m-0 py-2 my-fs">DeliveBoo</h1>
-        <h2 class="m-0 py-2 fs-1">Il sapore che ti raggiunge.</h2>
+        <h2 class=  "m-0 py-2 fs-1">Il sapore che ti raggiunge.</h2>
     </div>
 @endsection
