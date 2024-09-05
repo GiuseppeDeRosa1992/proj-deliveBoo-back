607de16b15db312e73a9d6f124f42cce81a17170
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-09-05 11:53:37 +0200

    added new type-restaurant and img new restaurant

--- database/seeders/RestaurantSeeder.php
+++ database/seeders/RestaurantSeeder.php
@@ -21,37 +21,6 @@ class RestaurantSeeder extends Seeder
         $PizzaNapoli->address = "Via dei Ciclamini 8";
         $PizzaNapoli->save();
 
-        // $LaVerace = new Restaurant();
-        // $LaVerace->user_id = 1;
-        // $LaVerace->name = "La Verace";
-        // $LaVerace->image = "https://picsum.photos/200/300";
-        // $LaVerace->p_iva = "12345678901";
-        // $LaVerace->address = "Via Rossi 22";
-        // $LaVerace->save();
-
-        // $IlFornoMagico = new Restaurant();
-        // $IlFornoMagico->user_id = 1;
-        // $IlFornoMagico->name = "Il Forno Magico";
-        // $IlFornoMagico->image = "https://picsum.photos/200/300";
-        // $IlFornoMagico->p_iva = "12345678901";
-        // $IlFornoMagico->address = "Via Genova 55";
-        // $IlFornoMagico->save();
-
-        // $BellaMargherita = new Restaurant();
-        // $BellaMargherita->user_id = 1;
-        // $BellaMargherita->name = "Bella Margherita";
-        // $BellaMargherita->image = "https://picsum.photos/200/300";
-        // $BellaMargherita->p_iva = "12345678901";
-        // $BellaMargherita->address = "Via Togliatti 73";
-        // $BellaMargherita->save();
-
-        // $PizzaECo = new Restaurant();
-        // $PizzaECo->user_id = 1;
-        // $PizzaECo->name = "Pizza & Co";
-        // $PizzaECo->image = "https://picsum.photos/200/300";
-        // $PizzaECo->p_iva = "12345678901";
-        // $PizzaECo->address = "Via Deri 23";
-        // $PizzaECo->save();
 
         $ElTacoLoco = new Restaurant();
         $ElTacoLoco->user_id = 2;
@@ -61,37 +30,6 @@ class RestaurantSeeder extends Seeder
         $ElTacoLoco->address = "Via Milano 69";
         $ElTacoLoco->save();
 
-        // $FiestaMexicana = new Restaurant();
-        // $FiestaMexicana->user_id = 2;
-        // $FiestaMexicana->name = "Fiesta Mexicana";
-        // $FiestaMexicana->image = "https://picsum.photos/200/300";
-        // $FiestaMexicana->p_iva = "12345678901";
-        // $FiestaMexicana->address = "Via Roma 78";
-        // $FiestaMexicana->save();
-
-        // $BurritoBrothers = new Restaurant();
-        // $BurritoBrothers->user_id = 2;
-        // $BurritoBrothers->name = "Burrito Brothers";
-        // $BurritoBrothers->image = "https://picsum.photos/200/300";
-        // $BurritoBrothers->p_iva = "12345678901";
-        // $BurritoBrothers->address = "Via Dei Gerani 43";
-        // $BurritoBrothers->save();
-
-        // $SaborMexicano = new Restaurant();
-        // $SaborMexicano->user_id = 2;
-        // $SaborMexicano->name = "Sabor Mexicano";
-        // $SaborMexicano->image = "https://picsum.photos/200/300";
-        // $SaborMexicano->p_iva = "12345678901";
-        // $SaborMexicano->address = "Via Livorno 33";
-        // $SaborMexicano->save();
-
-        // $CasaDelNacho = new Restaurant();
-        // $CasaDelNacho->user_id = 2;
-        // $CasaDelNacho->name = "Casa Del Nacho";
-        // $CasaDelNacho->image = "https://picsum.photos/200/300";
-        // $CasaDelNacho->p_iva = "12345678901";
-        // $CasaDelNacho->address = "Via Dei Pioppi 25";
-        // $CasaDelNacho->save();
 
         $SushiZen = new Restaurant();
         $SushiZen->user_id = 3;
@@ -101,37 +39,6 @@ class RestaurantSeeder extends Seeder
         $SushiZen->address = "Via Tokyo 32";
         $SushiZen->save();
 
-        // $WasabiHouse = new Restaurant();
-        // $WasabiHouse->user_id = 3;
-        // $WasabiHouse->name = "Wasabi House";
-        // $WasabiHouse->image = "https://picsum.photos/200/300";
-        // $WasabiHouse->p_iva = "12345678901";
-        // $WasabiHouse->address = "Via Berlino 24";
-        // $WasabiHouse->save();
-
-        // $SakuraSushi = new Restaurant();
-        // $SakuraSushi->user_id = 3;
-        // $SakuraSushi->name = "Sakura Sushi";
-        // $SakuraSushi->image = "https://picsum.photos/200/300";
-        // $SakuraSushi->p_iva = "12345678901";
-        // $SakuraSushi->address = "Via Monaco 71";
-        // $SakuraSushi->save();
-
-        // $OceanoBlu = new Restaurant();
-        // $OceanoBlu->user_id = 3;
-        // $OceanoBlu->name = "Oceano Blu";
-        // $OceanoBlu->image = "https://picsum.photos/200/300";
-        // $OceanoBlu->p_iva = "12345678901";
-        // $OceanoBlu->address = "Via Praga 45";
-        // $OceanoBlu->save();
-
-        // $NinjaRolls = new Restaurant();
-        // $NinjaRolls->user_id = 3;
-        // $NinjaRolls->name = "Ninja Rolls";
-        // $NinjaRolls->image = "https://picsum.photos/200/300";
-        // $NinjaRolls->p_iva = "12345678901";
-        // $NinjaRolls->address = "Via Amsterdam 78";
-        // $NinjaRolls->save();
 
         $DragoneDOro = new Restaurant();
         $DragoneDOro->user_id = 4;
@@ -141,42 +48,11 @@ class RestaurantSeeder extends Seeder
         $DragoneDOro->address = "Via Firenze 11";
         $DragoneDOro->save();
 
-        // $MandarinoRosso = new Restaurant();
-        // $MandarinoRosso->user_id = 4;
-        // $MandarinoRosso->name = "Mandarino Rosso";
-        // $MandarinoRosso->image = "https://picsum.photos/200/300";
-        // $MandarinoRosso->p_iva = "12345678901";
-        // $MandarinoRosso->address = "Via Dublino 56";
-        // $MandarinoRosso->save();
-
-        // $BambooGarden = new Restaurant();
-        // $BambooGarden->user_id = 4;
-        // $BambooGarden->name = "Bamboo Garden";
-        // $BambooGarden->image = "https://picsum.photos/200/300";
-        // $BambooGarden->p_iva = "12345678901";
-        // $BambooGarden->address = "Via Londra 46";
-        // $BambooGarden->save();
-
-        // $PeoniaImperiale = new Restaurant();
-        // $PeoniaImperiale->user_id = 4;
-        // $PeoniaImperiale->name = "Peonia Imperiale";
-        // $PeoniaImperiale->image = "https://picsum.photos/200/300";
-        // $PeoniaImperiale->p_iva = "12345678901";
-        // $PeoniaImperiale->address = "Via Rimini 25";
-        // $PeoniaImperiale->save();
-
-        // $SaporeDiShanghai = new Restaurant();
-        // $SaporeDiShanghai->user_id = 4;
-        // $SaporeDiShanghai->name = "Sapore Di Shanghai";
-        // $SaporeDiShanghai->image = "https://picsum.photos/200/300";
-        // $SaporeDiShanghai->p_iva = "12345678901";
-        // $SaporeDiShanghai->address = "Via Paolo Sarpi 49";
-        // $SaporeDiShanghai->save();
 
         $LaVerace = new Restaurant();
         $LaVerace->user_id = 5;
         $LaVerace->name = "La Verace";
-        $LaVerace->image = "https://picsum.photos/200/300";
+        $LaVerace->image = "https://raw.githubusercontent.com/GiuseppeDeRosa1992/immaginiDeliveboo-ristoranti-e-piatti/14bad662381859bdd1f55f4fe1de1bdd26d592e9/IMG/la-verace/la-verace.webp";
         $LaVerace->p_iva = "12345678901";
         $LaVerace->address = "Via Rossi 22";
         $LaVerace->save();
@@ -184,7 +60,7 @@ class RestaurantSeeder extends Seeder
         $PeccatiDiGola = new Restaurant();
         $PeccatiDiGola->user_id = 6;
         $PeccatiDiGola->name = "Peccati di Gola";
-        $PeccatiDiGola->image = "https://picsum.photos/200/300";
+        $PeccatiDiGola->image = "https://raw.githubusercontent.com/GiuseppeDeRosa1992/immaginiDeliveboo-ristoranti-e-piatti/af8858428c4d408841c01dd707defcbbe41fe35e/IMG/peccati-di-gola/peccati-di-gola.webp";
         $PeccatiDiGola->p_iva = "12345678901";
         $PeccatiDiGola->address = "Via Arrigo Rossi 22";
         $PeccatiDiGola->save();
@@ -192,7 +68,7 @@ class RestaurantSeeder extends Seeder
         $BrothersCafè = new Restaurant();
         $BrothersCafè->user_id = 7;
         $BrothersCafè->name = "Brother's Cafè";
-        $BrothersCafè->image = "https://picsum.photos/200/300";
+        $BrothersCafè->image = "https://raw.githubusercontent.com/GiuseppeDeRosa1992/immaginiDeliveboo-ristoranti-e-piatti/af8858428c4d408841c01dd707defcbbe41fe35e/IMG/brothers-cafe/brothers-cafe.webp";
         $BrothersCafè->p_iva = "12345678901";
         $BrothersCafè->address = "Via Trieste 22";
         $BrothersCafè->save();
@@ -200,7 +76,7 @@ class RestaurantSeeder extends Seeder
         $IlFornoMagico = new Restaurant();
         $IlFornoMagico->user_id = 8;
         $IlFornoMagico->name = "Il Forno Magico";
-        $IlFornoMagico->image = "https://picsum.photos/200/300";
+        $IlFornoMagico->image = "https://raw.githubusercontent.com/GiuseppeDeRosa1992/immaginiDeliveboo-ristoranti-e-piatti/14bad662381859bdd1f55f4fe1de1bdd26d592e9/IMG/il-forno-magico/forno-magico.webp";
         $IlFornoMagico->p_iva = "12345678901";
         $IlFornoMagico->address = "Via Genova 55";
         $IlFornoMagico->save();
@@ -208,7 +84,7 @@ class RestaurantSeeder extends Seeder
         $BurritoBrothers = new Restaurant();
         $BurritoBrothers->user_id = 9;
         $BurritoBrothers->name = "Burrito Brothers";
-        $BurritoBrothers->image = "https://picsum.photos/200/300";
+        $BurritoBrothers->image = "https://raw.githubusercontent.com/GiuseppeDeRosa1992/immaginiDeliveboo-ristoranti-e-piatti/14bad662381859bdd1f55f4fe1de1bdd26d592e9/IMG/burrito-brothers/burrito-brothers.webp";
         $BurritoBrothers->p_iva = "12345678901";
         $BurritoBrothers->address = "Via Roma 55";
         $BurritoBrothers->save();
@@ -216,7 +92,7 @@ class RestaurantSeeder extends Seeder
         $SaporeDiShanghai = new Restaurant();
         $SaporeDiShanghai->user_id = 10;
         $SaporeDiShanghai->name = "Sapore Di Shanghai";
-        $SaporeDiShanghai->image = "https://picsum.photos/200/300";
+        $SaporeDiShanghai->image = "https://raw.githubusercontent.com/GiuseppeDeRosa1992/immaginiDeliveboo-ristoranti-e-piatti/14bad662381859bdd1f55f4fe1de1bdd26d592e9/IMG/sapori-di-shangai/sapore-di-shanghai.webp";
         $SaporeDiShanghai->p_iva = "12345678901";
         $SaporeDiShanghai->address = "Via Pescara 85";
         $SaporeDiShanghai->save();
@@ -224,7 +100,7 @@ class RestaurantSeeder extends Seeder
         $OceanoBlu = new Restaurant();
         $OceanoBlu->user_id = 11;
         $OceanoBlu->name = "Oceano Blu";
-        $OceanoBlu->image = "https://picsum.photos/200/300";
+        $OceanoBlu->image = "https://raw.githubusercontent.com/GiuseppeDeRosa1992/immaginiDeliveboo-ristoranti-e-piatti/14bad662381859bdd1f55f4fe1de1bdd26d592e9/IMG/oceano-blu/Oceano-blu.webp";
         $OceanoBlu->p_iva = "12345678901";
         $OceanoBlu->address = "Via Praga 45";
         $OceanoBlu->save();
@@ -232,7 +108,7 @@ class RestaurantSeeder extends Seeder
         $LaBraceriaSottoCasa = new Restaurant();
         $LaBraceriaSottoCasa->user_id = 12;
         $LaBraceriaSottoCasa->name = "La Braceria Sotto Casa";
-        $LaBraceriaSottoCasa->image = "";
+        $LaBraceriaSottoCasa->image = "https://raw.githubusercontent.com/GiuseppeDeRosa1992/immaginiDeliveboo-ristoranti-e-piatti/af8858428c4d408841c01dd707defcbbe41fe35e/IMG/la-braceria-sotto-casa/la-braceria-sotto-casa.webp";
         $LaBraceriaSottoCasa->p_iva = "12345678901";
         $LaBraceriaSottoCasa->address = "Via Gramsci 33";
         $LaBraceriaSottoCasa->save();
