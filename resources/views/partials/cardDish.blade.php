de9e07930ea9542eaf5a3c36ffe2127c279085cd
francesco madonna <160396542+francescomadonna712@users.noreply.github.com>
2024-09-06 12:40:43 +0200

    modifica lingua e errore elimina piatto

--- resources/views/partials/cardDish.blade.php
+++ resources/views/partials/cardDish.blade.php
@@ -45,14 +45,14 @@
                             <div class="modal-content">
                                 <div class="modal-header">
                                     <h5 class="modal-title" id="modalTitle-{{ $dish->id }}">
-                                        Delete current dish
+                                        Elmina questo piatto
                                     </h5>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal"
                                         aria-label="Close"></button>
                                 </div>
 
                                 <div class="modal-body">
-                                    Elimina questo piatto: {{ $dish->name }}
+                                    Elimina questo piatto: <Strong>{{ $dish->name }}</Strong>
                                     ⚡Attento, non puoi annullare questa operazione
                                 </div>
                                 <div class="modal-footer justify-content-center">
