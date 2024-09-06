3a44ceecf57abe065ed9201c3dfd10ebf4d6dcd9
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-09-06 12:42:18 +0200

    added card resturant in dashboard.blade

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        //filtro i piatti tramite l'user loggato
+        $user = Auth::user();
+        //recupero con la variabile user i dati del ristorante associato ad ogni user
+        $data = [
+            'restaurants' => $user->restaurants()->orderByDesc('id')->get()
+        ];
+        return view('admin.dashboard', $data);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,23 +1,25 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container-fluid mt-4">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Pannello di Controllo') }}</div>
+	<div class="container-fluid mt-4">
+		<div class="row justify-content-center">
+			<div class="col-md-8">
+				<div class="card">
+					<div class="card-header">{{ __('Pannello di Controllo') }}</div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
+					<div class="card-body">
+						@if (session('status'))
+							<div class="alert alert-success" role="alert">
+								{{ session('status') }}
+							</div>
+						@endif
 
-                        {{ __('Sei Loggato!') }}
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+						{{ __('Sei Loggato!') }}
+					</div>
+
+					@include('partials.cardRestaurant')
+				</div>
+			</div>
+		</div>
+	</div>
 @endsection
