17be8b3450d348b4f02ce7ca612f37fb7adc84b8
francesco madonna <160396542+francescomadonna712@users.noreply.github.com>
2024-09-10 12:26:48 +0200

    aggiunto button torna indietro

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -4,7 +4,7 @@
     <div class="container">
         <h1>Ordini </h1>
 
-        @if($orders->isEmpty())
+        @if ($orders->isEmpty())
             <p>Nessun ordine disponibile.</p>
         @else
             <table class="table table-striped">
@@ -17,12 +17,12 @@
                         <th>Indirizzo</th>
                         <th>Data Ordine</th>
                         <th>Totale</th>
-                        
+
                         <th>Azioni</th>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach($orders as $order)
+                    @foreach ($orders as $order)
                         <tr>
                             <td>{{ $order->id }}</td>
                             <td>{{ $order->name_client }}</td>
@@ -31,13 +31,15 @@
                             <td>{{ $order->address_client }}</td>
                             <td>{{ $order->created_at->format('d/m/Y H:i') }}</td>
                             <td>€{{ number_format($order->total, 2) }}</td>
-                            
+
                             <td>
                                 <!-- <a href="{{ route('admin.orders.show', $order->id) }}" class="btn btn-primary">Visualizza</a> -->
-                                <form action="{{ route('admin.orders.destroy', $order->id) }}" method="POST" style="display:inline-block;">
+                                <form action="{{ route('admin.orders.destroy', $order->id) }}" method="POST"
+                                    style="display:inline-block;">
                                     @csrf
                                     @method('DELETE')
-                                    <button type="submit" class="btn btn-danger" onclick="return confirm('Sei sicuro di voler eliminare questo ordine?')">Elimina</button>
+                                    <button type="submit" class="btn btn-danger"
+                                        onclick="return confirm('Sei sicuro di voler eliminare questo ordine?')">Elimina</button>
                                 </form>
                             </td>
                         </tr>
@@ -45,6 +47,9 @@
                 </tbody>
             </table>
         @endif
+        <div class="text-center">
+            <a href="{{ route('admin.dashboard') }}" class="btn btn-outline-success my-2 py-1 px-3">Torna al ristorante
+                <i class="fa-solid fa-plate-wheat fa-lg fa-fw"></i></a>
+        </div>
     </div>
 @endsection
-
