c83557b3cde49d6c855e690b9b08cd7f7496f8b2
sidane17 <khalilsidane89@gmail.com>
2024-09-10 16:29:00 +0200

    button chart

--- resources/views/admin/orders/chart.blade.php
+++ resources/views/admin/orders/chart.blade.php
@@ -8,7 +8,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
+    
     <div class="container">
+        
         <h2 class="text-center my-3">Numero Di ordini Mensili</h2>
         <canvas id="monthChart" class="my-3"></canvas>
         <h2 class="text-center my-3">Totale Guadagni Mensili</h2>
