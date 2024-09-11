21a8a2cf5d23c9f1489d02f256e4976dfc112ae5
Lorenzo D'Addazio <ldaddazio11@gmail.com>
2024-09-11 15:10:33 +0200

    added stylish to orders.show

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -3,54 +3,30 @@
 @section('content')
 	<div class="container my-3">
 		<h1 class="text-center">Ordine n {{$order->id}}</h1>
-			<table class="table table-striped">
-				<thead>
-					<tr>
-						<th>Numero Identificativo Ordine</th>
-						<th>Nome Cliente</th>
-						<th>Email Cliente</th>
-						<th>Numero di Telefono</th>
-						<th>Indirizzo</th>
-						<th>Data Ordine</th>
-						<th>Dettagli Piatti</th>
-						<th>Totale</th>
 
+            <div class="card col-12 col-6-sm p-3">
+                <p>Numero Identificativo Ordine <strong>{{ $order->id }}</strong></p>
+                <p>Nome Cliente <strong>{{ $order->name_client }}</strong></p>
+                <p>Email Cliente <strong>{{ $order->email_client }}</strong></p>
+                <p>Numero di telefono Cliente <strong>{{ $order->number_phone }}</strong></p>
+                <p>Indirizzo di consegna <strong>{{ $order->address_client }}</strong></p>
+                <p>Ordine effettuato il <strong>{{ $order->created_at->format('d/m/Y H:i') }}</strong></p>
+                <div class="col-8">
+                    <ul>
+                        @foreach ($order->dishes as $dish)
+                            <li>
+                                <strong>{{ $dish->name }}</strong> - €<strong>{{ number_format($dish->price, 2) }}</strong> - Quantità:
+                                <strong>{{ $dish->pivot->quantity }}</strong>
+                            </li>
+                        @endforeach
+                    </ul>
+                    <p>Totale Ordine: €<strong>{{ number_format($order->total, 2) }}</strong></p>
+                </div>
 
-					</tr>
-				</thead>
-				<tbody>
-						<tr>
-							<td>{{ $order->id }}</td>
-							<td>{{ $order->name_client }}</td>
-							<td>{{ $order->email_client }}</td>
-							<td>{{ $order->number_phone }}</td>
-							<td>{{ $order->address_client }}</td>
-							<td>{{ $order->created_at->format('d/m/Y H:i') }}</td>
-
-							<td>
-								@if ($order->dishes && $order->dishes->isNotEmpty())
-									<ul>
-										@foreach ($order->dishes as $dish)
-											<li>
-												{{ $dish->name }} - €{{ number_format($dish->price, 2) }} - Quantità:
-												{{ $dish->pivot->quantity }}
-											</li>
-										@endforeach
-									</ul>
-								@else
-									<p>Nessun piatto associato a questo ordine.</p>
-								@endif
-							</td>
-							<td>€{{ number_format($order->total, 2) }}</td>
-							<td>
-
-							</td>
-						</tr>
-				</tbody>
-			</table>
-		<div class="text-center">
-			<a href="{{ route('admin.orders.index') }}" class="btn btn-outline-success my-2 py-1 px-3">Torna agli ordini
-				<i class="fa-solid fa-plate-wheat fa-lg fa-fw"></i></a>
-		</div>
+	        </div>
+            <div class="text-center">
+		        <a href="{{ route('admin.orders.index') }}" class="btn btn-outline-success my-2 py-1 px-3">Torna agli ordini
+		        <i class="fa-solid fa-truck-fast"></i></a>
+            </div>
 	</div>
 @endsection
